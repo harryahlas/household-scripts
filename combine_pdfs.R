a68d0eac327059be1192b2758bd109238352cb68
Harry Ahlas <42125399+harryahlas@users.noreply.github.com>
2020-03-08 19:00:48 -0700

    Update combine_pdfs.R

--- combine_pdfs.R
+++ combine_pdfs.R
@@ -9,3 +9,22 @@ pdf_files <- c("C:\\Users\\Anyone\\Desktop\\Scan1-4.pdf",
 pdf_output <- "C:\\Users\\Anyone\\Desktop\\ScanForVictoria.pdf"
 
 pdf_combine(pdf_files, pdf_output)
+
+
+library(mailR)
+send.mail(from = "___@hotmail.com",
+          to = c("___@aol.com"),
+          subject = "scans",
+          body = "scans from ____",
+          smtp = list(host.name = "smtp.live.com",  
+                      port = "587", 
+                      user.name = "_____@hotmail.com", 
+                      passwd = rstudioapi::askForPassword("password:"), 
+                      tls = TRUE
+          ),
+          authenticate = TRUE,
+          send = TRUE,
+          attach.files = pdf_output, 
+          #file.names = c("Download log.log", "Upload log.log", "DropBox File.rtf"), # optional parameter
+          #file.descriptions = c("Description for download log", "Description for upload log", "DropBox File"), # optional parameter
+          debug = TRUE)

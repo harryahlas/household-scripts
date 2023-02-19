c2e6c9b2d786b913f0200b2a561a18a8b11f6954
harryahlas <42125399+harryahlas@users.noreply.github.com>
2023-02-18 21:44:17 -0800

    SHELL add

---
+++ shell_henet.sh
@@ -0,0 +1,29 @@
+# this is to log in
+"C:\Program Files\RStudio\bin\msys-ssh-1000-18\ssh.exe" [LOGINNAME]@72.52.127.2
+
+#may need to run this
+ssh-keyscan -t rsa boron.he.net | out-file ~/.ssh/known_hosts -Append -Encoding ASCII;
+
+python3
+
+import os
+import keras
+import gensim
+
+
+for i in range(0,3):
+  print(i)
+
+
+mkmkmkm
+echo $env:SSH_ASKPASS
+echo $env:SSH_ASKPASS_REQUIRE
+
+ssh  edemise@72.52.127.2 rsa
+
+
+
+C:\Windows\System32\OpenSSH\ssh.exe edemise@72.52.127.2
+
+
+

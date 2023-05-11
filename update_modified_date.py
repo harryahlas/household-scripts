adbd6f4ef02afba34f942b40eddecee4429b2754
harryahlas <42125399+harryahlas@users.noreply.github.com>
2023-05-11 10:13:25 -0700

    subdirectories

--- update_modified_date.py
+++ update_modified_date.py
@@ -6,21 +6,25 @@ import datetime
 import pywintypes
 
 def change_file_time(path, new_time):
-    # Get the current file times
-    handle = win32file.CreateFile(
-        path, win32con.GENERIC_WRITE, 0, None, win32con.OPEN_EXISTING,
-        win32con.FILE_ATTRIBUTE_NORMAL, None)
+    try:
+        # Get the current file times
+        handle = win32file.CreateFile(
+            path, win32con.GENERIC_WRITE, 0, None, win32con.OPEN_EXISTING,
+            win32con.FILE_ATTRIBUTE_NORMAL, None)
 
-    # Change the last modified time
-    win32file.SetFileTime(handle, None, None, pywintypes.Time(new_time))
+        # Change the last modified time
+        win32file.SetFileTime(handle, None, None, pywintypes.Time(new_time))
 
-    handle.Close()
+        handle.Close()
+    except Exception as e:
+        print(f"Could not modify {path} due to error: {e}")
 
 def change_files_in_folder(folder_path, new_time):
-    for file_name in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file_name)
-        if os.path.isfile(file_path):
-            change_file_time(file_path, new_time)
+    for root, dirs, files in os.walk(folder_path):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            if os.path.isfile(file_path):
+                change_file_time(file_path, new_time)
 
 if __name__ == "__main__":
     folder_path = 'your folder path here'  # replace with your folder path

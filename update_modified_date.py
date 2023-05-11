967efae56ffd39a4e31c6301c7baa26370176995
harryahlas <42125399+harryahlas@users.noreply.github.com>
2023-05-11 10:02:52 -0700

    update

--- update_modified_date.py
+++ update_modified_date.py
@@ -1,24 +1,18 @@
 import os
 import time
-import pywintypes
 import win32file
 import win32con
+import datetime
+import pywintypes
 
-def convert_to_filetime(dt):
-    # Converts datetime to windows file time
-    microseconds = int(time.mktime(dt.timetuple())) * 1e6
-    return int(microseconds + 11644473600000000)  # Adding difference between 1970 and 1601
-
-def change_file_time(path, newtime):
-    newfiletime = convert_to_filetime(newtime)
-
+def change_file_time(path, new_time):
     # Get the current file times
     handle = win32file.CreateFile(
         path, win32con.GENERIC_WRITE, 0, None, win32con.OPEN_EXISTING,
         win32con.FILE_ATTRIBUTE_NORMAL, None)
 
     # Change the last modified time
-    win32file.SetFileTime(handle, None, None, pywintypes.FileTime(newfiletime))
+    win32file.SetFileTime(handle, None, None, pywintypes.Time(new_time))
 
     handle.Close()
 
@@ -29,7 +23,6 @@ def change_files_in_folder(folder_path, new_time):
             change_file_time(file_path, new_time)
 
 if __name__ == "__main__":
-    import datetime
     folder_path = 'your folder path here'  # replace with your folder path
-    new_time = datetime.datetime.now()  # replace with your desired time
+    new_time = datetime.datetime(2023, 5, 1, 10, 30)  # replace with your desired time
     change_files_in_folder(folder_path, new_time)
